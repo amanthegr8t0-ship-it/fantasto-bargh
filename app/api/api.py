f71091d751687c7ff40ad324e53d0c2aecc32859
Aman Sharma <amanthegr8t@gmail.com>
2026-05-27 14:50:53 +0530

    api fixing

--- app/api/api.py
+++ app/api/api.py
@@ -22,7 +22,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://fantasto-bargh.vercel.app"],
+    # allow_origins=["http://localhost:5173", "https://fantasto-bargh.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
